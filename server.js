3d0e54afb6f3bc3b8820bf0f8c5d20b7afffe029
toannguyen2603 <huutoan2603@gmail.com>
2021-11-20 23:58:02 +0700

    fix cors

--- server.js
+++ server.js
@@ -1,37 +1,44 @@
-const express = require('express');
-const connectDB = require('./config/connectDB');
+const express = require("express");
+const connectDB = require("./config/connectDB");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 
 const PORT = process.env.PORT || 8000;
 
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Connect database
 connectDB();
 
-app.get('/', (req, res) => {
-    res.json('Hello world!')
-})
+app.get("/", (req, res) => {
+  res.json("Hello world!");
+});
 
-// path 
-app.use('/api/', require('./routers/router'));
+// path
+app.use("/api/", require("./routers/router"));
 
 // use the express-static middleware
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-
-const specs = swaggerJsdoc(require('./api-docs/option'));
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs,{ explorer: true }));
-
+const specs = swaggerJsdoc(require("./api-docs/option"));
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true }),
+);
 
 app.listen(PORT, () => {
-    console.log(`Listening at: http://localhost:${PORT}`)
-})
-
-
+  console.log(`Listening at: http://localhost:${PORT}`);
+});
